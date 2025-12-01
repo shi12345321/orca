d58c27b431126494efacaa3ff194a9940aca1302
shi12345321 <anthonycostoy19@gmail.com>
2025-12-01 19:03:45 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,6 @@ import subprocess
 import datetime
 import zipfile
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -494,3 +488,4 @@ async def geoip(ctx, ip: str):
             await ctx.send(f"❌ Error: {e}")
 
 bot.run(TOKEN)
+
